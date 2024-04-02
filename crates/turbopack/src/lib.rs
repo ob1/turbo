c199a2e81942175d004f4124cebd0ea4a39e68f3
강동윤 (Donny) <kdy1997.dev@gmail.com>
2024-04-02 16:59:01 +0900

    fix more

--- crates/turbopack/src/lib.rs
+++ crates/turbopack/src/lib.rs
@@ -285,20 +285,25 @@ async fn apply_reexport_tree_shaking(
             module: final_module,
             export_name: new_export,
             ..
-        } = &*follow_reexports(module, export.clone_value(), side_effect_free_packages).await?;
+        } = &*follow_reexports(
+            module,
+            export.clone_value().into(),
+            side_effect_free_packages,
+        )
+        .await?;
         let module = if let Some(new_export) = new_export {
-            if *new_export == *export {
+            if **new_export == *export {
                 Vc::upcast(*final_module)
             } else {
                 Vc::upcast(EcmascriptModuleFacadeModule::new(
                     *final_module,
-                    ModulePart::renamed_export(new_export.clone(), export.clone_value()),
+                    ModulePart::renamed_export(new_export.clone(), export.clone_value().into()),
                 ))
             }
         } else {
             Vc::upcast(EcmascriptModuleFacadeModule::new(
                 *final_module,
-                ModulePart::renamed_namespace(export.clone_value()),
+                ModulePart::renamed_namespace(export.clone_value().into()),
             ))
         };
         return Ok(module);
@@ -415,7 +420,7 @@ impl ModuleAssetContext {
         let mut globs = Vec::with_capacity(pkgs.len());
 
         for pkg in pkgs {
-            globs.push(Glob::new(format!("**/node_modules/{{{}}}/**", pkg)));
+            globs.push(Glob::new(format!("**/node_modules/{{{}}}/**", pkg).into()));
         }
 
         Ok(Glob::alternatives(globs))
